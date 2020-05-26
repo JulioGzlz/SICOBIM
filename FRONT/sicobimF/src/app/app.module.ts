23ae86edda47bad9d699053557a8f6d3109cbd26
srlandero <landeroej@gmail.com>
2020-05-25 19:40:46 -0500

    Se formula el FRONT
    del login

--- FRONT/sicobimF/src/app/app.module.ts
+++ FRONT/sicobimF/src/app/app.module.ts
@@ -3,16 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
+    //provider un backend "ficticio para el front"
+    // fakeBackendProvider
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ FRONT/sicobimF/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../_models';
+import { AuthenticationService, UserService } from '../_services';
+import { first } from 'rxjs/operators';
+
+@Component({ 
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+
+  loading = false;
+  user: User;
+  userFromApi: User;
+
+  constructor(
+      private userService: UserService,
+      private authenticationService: AuthenticationService
+  ) {
+      this.user = this.authenticationService.userValue;
+  }
+
+  ngOnInit() {
+      this.loading = true;
+      this.userService.getById(this.user.id).pipe(first()).subscribe(user => {
+          this.loading = false;
+          this.userFromApi = user;
+      });
+  }
+
+}
