1e6578a247e247aea8d81ca96c2b67a5c68bc825
Edgar <landeroej@gmail.com>
2020-08-06 19:45:00 -0600

    Se modifica cadena de conexión

--- BACK/SICOBIM_B/Entities/User.cs
+++ BACK/SICOBIM_B/Entities/User.cs
@@ -61,18 +61,15 @@ namespace SICOBIM_B.Entities
         public int usuarioMod { get; set; }
         public DateTime fechaMod { get; set; }
         public bool activo { get; set; }
-<<<<<<< HEAD
 
        
       
-=======
         // [JsonIgnore]
         // public string Password { get; set; }
 
         [JsonIgnore]
         public List<RefreshToken> RefreshTokens { get; set; }
 
->>>>>>> Login
 
     }
 }
