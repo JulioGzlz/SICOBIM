521efddc176ec748a5bad2cd89a0663934c51a9c
Edgar <landeroej@gmail.com>
2020-09-27 14:57:10 -0500

    Se complementa FRONT

--- FRONT/SABICI/src/app/app.module.ts
+++ FRONT/SABICI/src/app/app.module.ts
@@ -1,4 +1,4 @@
-﻿import { NgModule } from '@angular/core';
+﻿import { NgModule, APP_INITIALIZER } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -6,13 +6,14 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+import { AppComponent } from './app.component';
+import { AlertComponent } from './_components';
 import { HomeComponent } from './home';
-import { AdminComponent } from './admin';
-import { LoginComponent } from './login';
+import { AuthenticationService } from './_services/authentication.service';
+import { appInitializer } from './_helpers/app.initializer';
+import { LoginComponent } from './account/login.component';
 
 @NgModule({
     imports: [
@@ -23,18 +24,18 @@ import { LoginComponent } from './login';
     ],
     declarations: [
         AppComponent,
+        AlertComponent,
         HomeComponent,
-        AdminComponent,
-        LoginComponent
+        LoginComponent,
     ],
     providers: [
+        { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [AuthenticationService] },
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
         { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
 
         // provider used to create fake backend
-        // fakeBackendProvider
+        //fakeBackendProvider
     ],
     bootstrap: [AppComponent]
 })
-
-export class AppModule { }
+export class AppModule { };
