e9730344c69d74946bc53f7db659a147a2167d35
Edgar <landeroej@gmail.com>
2020-10-18 13:27:37 -0500

    Se modifican controller para lo service

--- BACK/SICOBIM_B/Controllers/CatPerfilController.cs
+++ BACK/SICOBIM_B/Controllers/CatPerfilController.cs
@@ -39,97 +39,82 @@ namespace SICOBIM_B.Controllers
             var result = _businessPerfiles.getCatArea();
             return Ok(result);
         }
-        //[HttpGet("GetCatEstadoDelBien")]
-        //public IActionResult GetCatEstadoDelBien()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatEstadoDelBien();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatGarantia")]
-        //public IActionResult GetCatGarantia()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatGarantia();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatPermiso")]
-        //public IActionResult GetCatPermiso()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatPermiso();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatPisos")]
-        //public IActionResult GetCatPisos()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatPisos();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatRol")]
-        //public IActionResult GetCatRol()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatRol();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatServicio")]
-        //public IActionResult GetCatServicio()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatServicio();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatTipoContrato")]
-        //public IActionResult GetCatTipoContrato()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatTipoContrato();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatTipoDeBien")]
-        //public IActionResult GetCatTipoDeBien()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatTipoDeBien();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatTipoEntrada")]
-        //public IActionResult GetCatTipoEntrada()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatTipoEntrada();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatTipoPartida")]
-        //public IActionResult GetCatTipoPartida()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatTipoPartida();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatTurno")]
-        //public IActionResult GetCatTurno()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatTurno();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatUnidadDestino")]
-        //public IActionResult GetCatUnidadDestino()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatUnidadDestino();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetCatTipoSalida")]
-        //public IActionResult GetCatTipoSalida()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatTipoSalida();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetTblAreaServicio")]
-        //public IActionResult GetTblAreaServicio()
-        //{
-        //    var result = _catalogoPerfilesService.GetTblAreaServicio();
-        //    return Ok(result);
-        //}
-        //[HttpGet("GetTblBienes")]
-        //public IActionResult GetTblBienes()
-        //{
-        //    var result = _catalogoPerfilesService.GetCatTipoSalida();
-        //    return Ok(result);
-        //}
+        [HttpGet("GetCatEstadoDelBien")]
+        public IActionResult GetCatEstadoDelBien()
+        {
+            var result = _businessPerfiles.getEstadoBien();
+            return Ok(result);
+        }
+        [HttpGet("GetCatGarantia")]
+        public IActionResult GetCatGarantia()
+        {
+            var result = _businessPerfiles.getCatGarantia();
+            return Ok(result);
+        }
+        [HttpGet("GetCatPermiso")]
+        public IActionResult GetCatPermiso()
+        {
+            var result = _businessPerfiles.getCatPermiso();
+            return Ok(result);
+        }
+        [HttpGet("GetCatPisos")]
+        public IActionResult GetCatPisos()
+        {
+            var result = _businessPerfiles.getCatPermiso();
+            return Ok(result);
+        }
 
+        [HttpGet("GetCatServicio")]
+        public IActionResult GetCatServicio()
+        {
+            var result = _businessPerfiles.getServicios();
+            return Ok(result);
+        }
+
+
+        [HttpGet("GetCatTipoContrato")]
+        public IActionResult GetCatTipoContrato()
+        {
+            var result = _businessPerfiles.getCatPisos();
+            return Ok(result);
+        }
+        [HttpGet("GetCatTipoDeBien")]
+        public IActionResult GetCatTipoDeBien()
+        {
+            var result = _businessPerfiles.getTipoBienes();
+            return Ok(result);
+        }
+        [HttpGet("GetCatTipoEntrada")]
+        public IActionResult GetCatTipoEntrada()
+        {
+            var result = _businessPerfiles.getTipoEntrada();
+            return Ok(result);
+        }
+        [HttpGet("GetCatTipoPartida")]
+        public IActionResult GetCatTipoPartida()
+        {
+            var result = _businessPerfiles.getTipoPartidad();
+            return Ok(result);
+        }
+        [HttpGet("GetCatTurno")]
+        public IActionResult GetCatTurno()
+        {
+            var result = _businessPerfiles.getCatTurnos();
+            return Ok(result);
+        }
+        [HttpGet("GetCatUnidadDestino")]
+        public IActionResult GetCatUnidadDestino()
+        {
+            var result = _businessPerfiles.getUnidadDestino();
+            return Ok(result);
+        }
+        [HttpGet("GetCatTipoSalida")]
+        public IActionResult GetCatTipoSalida()
+        {
+            var result = _businessPerfiles.getTipoSalida();
+            return Ok(result);
+        }
+       
 
     }
     
