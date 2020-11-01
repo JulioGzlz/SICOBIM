810b144d7491e002e875d46a8b7bb564f91eb3b7
JulioGzlz <jcesargon7@gmail.com>
2020-10-31 21:17:59 -0600

    se agregaron campos

--- BACK/SICOBIM_B/Entities/TblConfPerfil.cs
+++ BACK/SICOBIM_B/Entities/TblConfPerfil.cs
@@ -10,17 +10,19 @@ namespace SICOBIM_B.Entities
     [Table("TblConfPerfil")]
     public class TblConfPerfil
     {
-
-        public DateTime fechaAlta { get; set; }
-        public int usuarioMod { get; set; }
-        public DateTime fechaMod { get; set; }
-        public bool activo { get; set; }
-
         [ForeignKey("idRol")]
         public virtual CatRol CatRol { get; set; }
 
         [ForeignKey("idPermiso")]
+        public bool activo { get; set; }
         public virtual CatPermiso CatPermiso { get; set; }
+        public int idUsuarioAlta { get; set; }
+        public DateTime fechaAlta { get; set; }
+        public int usuarioMod { get; set; }
+        public DateTime fechaMod { get; set; }
+
+
+
     }
 }
 
---
+++ BACK/SICOBIM_B/Migrations/20201101031713_jzlz_24.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SICOBIM_B.Migrations
+{
+    public partial class jzlz_24 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TblConfPerfil_CatPermiso_idPermiso",
+                table: "TblConfPerfil");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TblConfPerfil_idPermiso",
+                table: "TblConfPerfil");
+
+            migrationBuilder.DropColumn(
+                name: "idPermiso",
+                table: "TblConfPerfil");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CatPermisoid",
+                table: "TblConfPerfil",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "idUsuarioAlta",
+                table: "TblConfPerfil",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TblConfPerfil_CatPermisoid",
+                table: "TblConfPerfil",
+                column: "CatPermisoid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TblConfPerfil_CatPermiso_CatPermisoid",
+                table: "TblConfPerfil",
+                column: "CatPermisoid",
+                principalTable: "CatPermiso",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TblConfPerfil_CatPermiso_CatPermisoid",
+                table: "TblConfPerfil");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TblConfPerfil_CatPermisoid",
+                table: "TblConfPerfil");
+
+            migrationBuilder.DropColumn(
+                name: "CatPermisoid",
+                table: "TblConfPerfil");
+
+            migrationBuilder.DropColumn(
+                name: "idUsuarioAlta",
+                table: "TblConfPerfil");
+
+            migrationBuilder.AddColumn<int>(
+                name: "idPermiso",
+                table: "TblConfPerfil",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TblConfPerfil_idPermiso",
+                table: "TblConfPerfil",
+                column: "idPermiso");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TblConfPerfil_CatPermiso_idPermiso",
+                table: "TblConfPerfil",
+                column: "idPermiso",
+                principalTable: "CatPermiso",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
